14f5ca33cbe130fe94e0e6912e19a8d0423166d2
samitamerarar <sami.t.arar@hotmail.com>
2024-02-04 17:09:05 -0500

    Added Logout endpoint

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from flask_jwt_extended import JWTManager
 
 
 from db import db
+from blocklist import BLOCKLIST
 import models  # calls __init__
 
 from resources.user import blp as UserBlueprint
@@ -35,17 +36,25 @@ def create_app(db_url=None):
     app.config["JWT_SECRET_KEY"] = "user1"
     jwt = JWTManager(app)
 
+    @jwt.token_in_blocklist_loader
+    def check_if_token_in_blocklist(jwt_header, jwt_payload):
+        return jwt_payload["jti"] in BLOCKLIST
+
+    @jwt.revoked_token_loader
+    def revoked_token_callback(jwt_header, jwt_payload):
+        return (jsonify({"description": "The token has been revoked.", "error": "token_revoked"}), 401)
+
     @jwt.expired_token_loader
     def expired_token_callback(jwt_header, jwt_payload):
         return (jsonify({"message": "The token has expired.", "error": "token_expired"}), 401)
 
     @jwt.invalid_token_loader
     def invalid_token_callback(error):
-        return (jsonify({"message": "Signature verification failed.", "error": "invalid_token"}), 401,)
+        return (jsonify({"message": "Signature verification failed.", "error": "invalid_token"}), 401)
 
     @jwt.unauthorized_loader
     def missing_token_callback(error):
-        return (jsonify({"description": "Request does not contain an access token.", "error": "authorization_required", }), 401, )
+        return (jsonify({"description": "Request does not contain an access token.", "error": "authorization_required"}), 401)
 
     # Create DB tables
     with app.app_context():
--- resources/user.py
+++ resources/user.py
@@ -1,11 +1,12 @@
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from passlib.hash import pbkdf2_sha256
-from flask_jwt_extended import create_access_token
+from flask_jwt_extended import create_access_token, get_jwt, jwt_required
 
 from db import db
 from models import UserModel
 from schemas import UserSchema
+from blocklist import BLOCKLIST
 
 
 blp = Blueprint("Users", "users", description="Operations on users")
@@ -57,3 +58,12 @@ class UserLogin(MethodView):
             return {"access_token": access_token}, 200
 
         abort(401, message="Invalid credentials.")
+
+
+@blp.route("/logout")
+class UserLogout(MethodView):
+    @jwt_required()
+    def post(self):
+        jti = get_jwt()["jti"]
+        BLOCKLIST.add(jti)
+        return {"message": "Successfully logged out"}, 200
