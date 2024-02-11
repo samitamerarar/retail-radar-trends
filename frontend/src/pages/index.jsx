46e02e132ee8fb251eb4dfd1154c6c349a20a3fa
samitamerarar <sami.t.arar@hotmail.com>
2024-02-10 21:55:51 -0500

    unused imports

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -1,7 +1,5 @@
 import Head from 'next/head'
 
-import { Footer } from '@/components/Footer'
-import { Header } from '@/components/Header'
 import { Hero } from '@/components/Hero'
 
 export default function Home() {
